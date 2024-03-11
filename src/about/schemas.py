4932e4f5770336cbbc2dad5714b4515fd463b2ee
atelay <deadroll95@gmail.com>
2024-03-11 18:13:14 +0200

    add title field for about page

--- src/about/schemas.py
+++ src/about/schemas.py
@@ -5,9 +5,11 @@ from pydantic import (
 
 from .models import About
 
+TITLE_LEN = About.title.type.length
 CONTENT_LEN = About.content.type.length
 
 
 class AboutSchema(BaseModel):
     id: int = Field(..., ge=1)
+    title: str = Field(max_length=TITLE_LEN)
     content: str = Field(max_length=CONTENT_LEN)
